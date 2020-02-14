b3970b30e6d80fdb18c16738ad5be2fa66bcf2ba
biddls <biddls@users.noreply.github.com>
2020-02-14 02:48:33 +0000

    data and image injestion

--- train.py
+++ train.py
@@ -1,31 +1,51 @@
-import tensorflow as tf
+#import tensorflow as tf
 import glob, os
 from time import sleep
+import time
+import pandas as pd
+from PIL import Image as Image
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+labels = {'T': 0,
+         'CT':1}
 
 pool_sizes = [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30, 40, 60, 120]
-#use the CSV then in vott
-
-os.chdir("dat saved/CS-GO-TFRecords-export")
-files = glob.glob("*.tfrecord")
-
-files = tf.data.TFRecordDataset(files)
-
-# The following functions can be used to convert a value to a type compatible
-# with tf.Example.
-def _bytes_feature(value):
-    """Returns a bytes_list from a string / byte."""
-    if isinstance(value, type(tf.constant(0))):
-        value = value.numpy()  # BytesList won't unpack a string from an EagerTensor.
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-for x in files:
-    x = _bytes_feature(x)
 
 
-print(type(files))
+os.chdir("dat saved/vott-csv-export")
+file = glob.glob("*.csv")
 
-sleep(1)
-for x in files:
-    print(type(x))
-    x = tf.train.Feature(bytes_list = x)
-    break
+dat = pd.read_csv(file[0])
+dat['label'] = dat['label'].map(labels)
+
+
+images = dat['image']
+dat = dat.drop(columns = ['image']).astype('int')
+
+
+images = images.values
+data = dat.values
+
+
+for index, iter in enumerate(images):
+    images[index] = np.array(Image.open(iter).convert('RGB'))
+    #plt.imshow(images[index])
+    #plt.show()
+
+print(len(images),'len')
+print(images.shape,'shape')
+
+
+print(images[len(images)-1]#image no. 0 to len -1
+      [1079]#row 0-1079
+      [1919]#column 0-1919
+      [2])#rgb 0-2
+
+print(images[0]#image no. 0 to len -1
+      [0]#row 0-1079
+      [0]#column 0-1919
+      [0])#rgb 0-2
+
+print(type(images))

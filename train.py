02529de504b83a1d9270c74a2245de59824276b1
biddls <biddls@users.noreply.github.com>
2020-02-17 14:17:40 +0000

    data aranbgement

--- train.py
+++ train.py
@@ -22,24 +22,27 @@ dat['label'] = dat['label'].map(labels)
 
 
 images = dat['image']
-dat = dat.drop(columns = ['image']).astype('int')
+#images.insert(1,'names',dat['image'], True)
+#dat = dat.drop(columns = ['image']).astype('int')
 
+dat = dat[['xmin', 'ymin', 'xmax', 'ymax', 'label', 'image']]
 
 images = images.values
 data = dat.values
 
-
 for index, iter in enumerate(images):
-    images[index] = np.array(Image.open(iter).convert('RGB'))
+    images[index] = [np.array(Image.open(iter).convert('RGB')),iter]
     #plt.imshow(images[index])
     #plt.show()
 
 print(images[len(images)-1]#image no. 0 to len -1
+      [0]
       [1079]#row 0-1079
       [1919]#column 0-1919
       [2])#rgb 0-2
 
 print(images[0]#image no. 0 to len -1
+      [0]
       [0]#row 0-1079
       [0]#column 0-1919
       [0])#rgb 0-2
@@ -54,8 +57,24 @@ print('xmin,ymin,xmax,ymax,label')
 print(data[0])
 
 for x in data:
-    b.extend([[int(x[4]), int((x[0]+x[2])/2), int((x[1]+x[3])/2), int((x[2] - x[0])/2), int((x[3] - x[1])/2)]])
+    b.extend([[int(x[4]), int((x[0]+x[2])/2), int((x[1]+x[3])/2), int((x[2] - x[0])/2), int((x[3] - x[1])/2), x[-1]]])
 
-print('label\tXcenter\tYcenter\tWidthfromC\tHightfromC\t')
+print('label\tXcenter\tYcenter\tWidthfromC\tHightfromC\timage name')
 for b in b[0:3]:
     print(b)
+
+#split images up and linking data
+#top left to bottom right
+
+box_size = 120
+
+for image in images:
+    plt.imshow(image[0])
+    plt.show()
+    break
+
+for image in images:
+    for dat in data:
+        if dat[-1] == image[-1]:
+            print("huzzar a man of Quality")
+            #do cheks here nad re-arange stuff into 1 data form

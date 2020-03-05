51f6dc14789656792b4c35b88d8935a6792979d8
biddls <biddls@users.noreply.github.com>
2020-03-05 14:22:09 +0000

    re wrote to be faster & more flexible

--- getdat.py
+++ getdat.py
@@ -17,35 +17,36 @@ else:
     training_data = []
 
 def main(fps):
+
+    if fps == None:
+        fps = 0
+    else:
+        fps = 1/fps
+
     for i in range(4,0,-1):
         print(i)
         time.sleep(1)
 
     paused = False
-    framtimes = []
+    ind = time.time()
 
     while True:
-        start = time.time()
-        if not paused:
+
+        if not paused and time.time() >= (ind + fps):
+            print('in')
+            ind += 1
             # 800x600 windowed mode
-            screen = grab_screen(region=(0, 40, 800, 640))
+            screen = grab_screen(region=(1, 26, 800 ,625))
+            cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY))
             screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-            #screen = cv2.resize(screen, (160, 120))
-            # resize to something a bit more acceptable for a CNN
             training_data.append([screen])
 
-            if len(training_data) % 1000 == 0:
+            if len(training_data) % 100 == 0:
                 print(len(training_data))
                 np.save(file_name, training_data)
 
-            finish = time.time()
-            if fps != None and (1/fps) > (finish - start):
-                sleep((1 / fps) - (finish - start))
-                framtimes.append(1 / (((1 / fps) - (finish - start)) + (finish - start)))
-            else:
-                framtimes.append(1 / (finish - start))
-
-            print(round(mean(framtimes)), 'FPS')
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
         keys = key_check()
         if 'T' in keys:
@@ -57,10 +58,9 @@ def main(fps):
                 print('Pausing!')
                 paused = True
                 time.sleep(1)
-
-fps = None
+fps = 0
 try:
-    fps = int(input('fps: '))
+    fps = float(input('fps: '))
 except:
     pass
 main(fps)
